e58cd53c4e9f0035a6e6fd98410285519c3b562f
AvatarJamon <59138407+Hiro016D@users.noreply.github.com>
2020-10-13 18:53:17 +0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Weeb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Cockeru bro...";
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.BackgroundColor = ConsoleColor.DarkBlue;
+            Console.WindowHeight = 40;
+            Console.WriteLine("I love Cockeru!");
+
+            Console.ReadLine();
+            
+            Console.WriteLine("My name is Hiro. I love myself some delicious cockeru.");
+            Console.WriteLine("What is your favorite Color");
+
+            Console.ReadLine();
+
+            Console.WriteLine("Cool. Mine is Light Blue!");
+
+            Console.ReadKey();
+        }
+    }
+}
