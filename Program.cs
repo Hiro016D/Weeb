a708185e957abdf2040755ecd8038a0bbbda5041
AvatarJamon <59138407+Hiro016D@users.noreply.github.com>
2020-10-15 09:48:05 +0400

    3 number average calc

--- Program.cs
+++ Program.cs
@@ -6,17 +6,21 @@ namespace Weeb
     {
         static void Main(string[] args)
         {
-            int num01;
-            int num02;
+            double num01;
+            double num02;
+            double num03;
 
             Console.Write("Input a number: ");
-            num01 = Convert.ToInt32( Console.ReadLine() );
+            num01 = Convert.ToDouble( Console.ReadLine() );
 
-            Console.Write("Input a second number");
-            num02 = Convert.ToInt32( Console.ReadLine() );
+            Console.Write("Input a second number: ");
+            num02 = Convert.ToDouble( Console.ReadLine() );
+
+            Console.Write("Input a third number: ");
+            num03 = Convert.ToDouble( Console.ReadLine() );
             
-            int result = num01 * num02;
-            Console.Write("The result is " + result);
+            double result = num01 + num02 + num03;
+            Console.Write("The average of these numbers is " + result / 3);
 
             //wait's before closing terminal
             Console.ReadKey();
